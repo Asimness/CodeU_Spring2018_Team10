2bc5e7ef13682796e5013a36efa984353d8f9998
Asim Anis <asimness@gmail.com>
2018-05-30 18:26:30 -0500

    Update RegisterServlet.java
    Fixed comments / documentation

--- src/main/java/codeu/controller/RegisterServlet.java
+++ src/main/java/codeu/controller/RegisterServlet.java
@@ -48,7 +48,7 @@ public class RegisterServlet extends HttpServlet {
   }
   
   /**
-   * Sets the UserStore used by this servlet. This function provides a common setup method for use
+   * Sets the ActivityStore used by this servlet. This function provides a common setup method for use
    * by the test framework or the servlet's init() function.
    */
   void setActivityStore(ActivityStore activityStore) {
