3b129ff798858f36dbca942ba43a397f60ce1083
Asim Anis <asimness@gmail.com>
2018-07-14 12:55:11 -0500

    Sentiment_Analysis_Dependencies

---
+++ src/main/java/codeu/model/data/SA.java
@@ -0,0 +1,14 @@
+package codeu.model.data;
+
+import com.paralleldots.paralleldots.App;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+
+public class SA {
+
+	App pd = new App("rwJj7ySstJht0QfJUQ0sKBErzeavAM6ydGcw8LQGLWE");
+	
+	
+	
+}
